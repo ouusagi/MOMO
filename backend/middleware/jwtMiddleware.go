f59853b0fa5932bcc8eee6d7d9b63c316f133876
ouusagi <xmflzhqxj123@naver.com>
2026-06-11 15:53:52 +0900

    feat:add JwtMiddleware

--- backend/middleware/jwtMiddleware.go
+++ backend/middleware/jwtMiddleware.go
@@ -1,1 +1,45 @@
 package middleware
+
+import (
+	"momo/utils"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JwtMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		// 헤더값 가져오기
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "토큰이 없습니다"})
+			c.Abort()
+			return
+		}
+
+		// 토큰값 출력
+		parts := strings.Split(authHeader, " ")
+		if len(parts) != 2 || parts[0] != "Bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "토큰 형식이 올바르지 않습니다"})
+			c.Abort()
+			return
+		}
+
+		tokenString := parts[1]
+
+		// 토큰 검증
+		claims, err := utils.ValidateToken(tokenString)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "유효하지 않은 토큰입니다"})
+			c.Abort()
+			return
+		}
+
+		// user_id 추출 후 저장
+		userID := uint(claims["user_id"].(float64))
+		c.Set("user_id", userID)
+		c.Next()
+	}
+}
