88d21518374605ece82210533fe5884e57b24b24
ouusagi <xmflzhqxj123@naver.com>
2026-06-15 15:03:54 +0900

    feat:add routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,1 +1,27 @@
 package routes
+
+import (
+	"momo/controllers"
+	"momo/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	r.POST("/api/signup", controllers.Signup)
+	r.POST("/api/login", controllers.Login)
+
+	auth := r.Group("/api")
+
+	auth.Use(middleware.JwtMiddleware())
+	{
+		auth.POST("/expenses", controllers.CreateExpense)
+		auth.GET("/expenses", controllers.GetExpense)
+		auth.PUT("/expenses/:id", controllers.UpdateExpense)
+		auth.DELETE("/expenses/:id", controllers.DeleteExpense)
+	}
+
+	return r
+}
