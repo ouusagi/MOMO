13e9fda1938be6e4baccf28a07169bd194c11852
ouusagi <xmflzhqxj123@naver.com>
2026-07-28 14:32:24 +0900

    fix: Categoryicon ui

--- frontend/src/components/common/CategoryIcon.tsx
+++ frontend/src/components/common/CategoryIcon.tsx
@@ -10,13 +10,13 @@ interface CategoryIconProps {
 
 const CategoryIcon = ({emoji, label, onClick, active = false, amount}:CategoryIconProps) => {
 
-    const CategoryIconBox = 'rounded-3xl flex flex-col items-center gap-1 cursor-pointer bg-opacity-65 p-2 transition-colors w-24 h-28'
+    const CategoryIconBox = 'rounded-2xl flex flex-col items-center cursor-pointer bg-opacity-65 p-1 transition-colors w-24 h-20'
     const activebgColor = active ? 'bg-[#F47560]' : 'bg-[#FFFFFF]'
     const activelabelColor = active ? 'text-white' : 'text-[#3D2C2C]'
 
     return(
         <div className={`${CategoryIconBox} ${activebgColor}`} onClick={onClick}>
-            <span className="text-2xl">{emoji}</span>
+            <span className="text-base">{emoji}</span>
             <span className={`${activelabelColor} font-bold text-base`}>{label}</span>
             {amount !== undefined && (<span className="text-[#F47560] font-bold">{amount.toLocaleString()} 円</span>)}
         </div>
