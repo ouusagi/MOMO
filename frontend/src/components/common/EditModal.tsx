3b31b4fb24c2abb9fa97a1c9b99f8a7a773b3cab
ouusagi <xmflzhqxj123@naver.com>
2026-08-15 16:37:01 +0900

    feat:add update-transaction

---
+++ frontend/src/components/common/EditModal.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react'
+import Button from './Button'
+import Input from './Input'
+import inputIcon from '../../assets/inputicon.png'
+import DatePickerModal from './DatePickerModal'
+
+interface EditModalProps {
+    isOpen: boolean
+    onClose: () => void
+    onSave?: (data: { title: string; amount: number; memo: string; expenseDate: string; }) => void
+    initialData?: {title: string; amount: number; memo: string; date: string;}
+}
+
+const EditModal = ({ isOpen, onClose, onSave, initialData }: EditModalProps) => {
+
+    const [title, setTitle] = useState(initialData.title)
+    const [amount, setAmount] = useState(initialData.amount.toString())
+    const [memo, setMemo] = useState(initialData.memo || '')
+    const [show, setShow] = useState(false)
+    const [selectedDate, setSelectedDate] = useState(new Date(initialData?.date))
+    const [dateModal, setDateModal] = useState(false)
+
+    useEffect(()=>{
+        if(isOpen){
+            setTimeout(() => setShow(true),10)
+        }
+        else{setShow(false)}
+    },[isOpen])
+
+    if (!isOpen) return null
+
+    const handleSave = () => {
+        onSave({title, amount: Number(amount.replaceAll(',', '')), memo, expenseDate:selectedDate.toISOString()})
+    }
+
+    return (
+        <div className='fixed inset-0 bg-black bg-opacity-30 flex items-end justify-center z-50'>
+            <div className='absolute inset-0' onClick={onClose} />
+
+            <div className={`relative bg-[#FFD9CE] rounded-t-3xl p-6 w-full flex flex-col gap-4 shadow-lg transition-transform duration-300 ${show ? 'translate-y-0' : 'translate-y-full'}`}>
+
+                {/* header */}
+                <div className='flex items-center justify-between mb-2'>
+                    <Button className="" variant='back' onClick={onClose}>←</Button>
+                    <span className='text-[#3D2C2C] font-bold'>支出修正</span>
+                    <span className='text-[#F47560] font-bold cursor-pointer' onClick={handleSave}>保存</span>
+                </div>
+
+                {/* input */}
+                <Input label='店名' type='text' value={title} onChange={(e) => setTitle(e.target.value)} placeholder='店名'/>
+
+                <div className='flex flex-col gap-1'>
+                    <label className='text-[#7A5555] font-bold'>金額</label>
+                    <input
+                        type='text'
+                        value={amount === '' ? '' : Number(amount.replaceAll(',', '')).toLocaleString()}
+                        onChange={(e) => {
+                            const raw = e.target.value.replaceAll(',', '')
+                            if (/^\d*$/.test(raw)) setAmount(raw)
+                        }}
+                        placeholder='0'
+                        className='rounded-xl outline-none border-none py-4 px-3 bg-[#FFFFFF] bg-opacity-75 text-[#3D2C2C]'
+                    />
+                </div>
+
+                <Input label='メモ' type='text' value={memo} onChange={(e) => setMemo(e.target.value)} placeholder='メモを入力してください'/>
+
+                {/* Date */}
+                <div className='bg-white bg-opacity-60 rounded-2xl px-4 py-3 mt-3 flex items-center justify-between' onClick={()=> setDateModal(true)}>
+                    <div>
+                        <p className='text-[#B89090] text-xs'>日付 (タップして編集)</p>
+                        <p className='text-[#3D2C2C] font-bold text-sm'>{selectedDate.toLocaleDateString('ja-JP')}</p>
+                    </div>
+                    <img src={inputIcon} alt="input_icon" className="w-8" />
+                </div>
+
+                <Button variant='primary' fullWidth onClick={handleSave} className='mt-2 pb-4'>保存する</Button>
+
+                <DatePickerModal open={dateModal} selectedDate={selectedDate} onChange={setSelectedDate} onClick={() => setDateModal(false)}/>
+            </div>
+        </div>
+    )
+}
+
+export default EditModal
--- frontend/src/pages/TransactionDetailPage.tsx
+++ frontend/src/pages/TransactionDetailPage.tsx
@@ -1,10 +1,11 @@
 import { useNavigate, useParams } from "react-router-dom"
 import Button from "../components/common/Button"
 import { categoryEmoji } from "../constants/categoryEmoji"
-import { useDeleteExpense, useGetExpenses } from "../hooks/useExpense"
+import { useDeleteExpense, useGetExpenses, useUpdateExpense } from "../hooks/useExpense"
 import ConfirmModal from "../components/common/ConfirmModal"
 import { useState } from "react"
 import toast from "react-hot-toast"
+import EditModal from "../components/common/EditModal"
 
 
 
@@ -16,12 +17,20 @@ const TransactionDetailPage = () => {
     const navigate = useNavigate()
     const filterExpenses = ExpensesData?.find(item => item.id === Number(id))
     const [modalOpen, setModalOpen] = useState(false)
+    const [editModalOpen, setEditModalOpen] = useState(false)
     const { mutate:deleteExpense } = useDeleteExpense()
+    const { mutate:updateExpense } = useUpdateExpense()
 
     const handleDelete = () => {
         deleteExpense(filterExpenses?.id, {
             onSuccess:() => {toast.success("削除が完了しました。"); navigate('/transactions')},
             onError:() => {toast.error("エラーが発生しました")},
+        })}
+
+    const handleUpdate = (data: { title: string; amount: number; memo: string; expenseDate:string; }) => {
+        updateExpense({id:Number(id), data:{title:data?.title, amount:data?.amount, memo:data?.memo, expenseDate:data?.expenseDate, category:filterExpenses?.category}},{
+        onSuccess:()=>{toast.success("支出を更新しました。"); navigate('/transactions')},
+        onError:()=>{toast.error("エラーが発生しました")}
         })}
 
 
@@ -37,12 +46,12 @@ const TransactionDetailPage = () => {
             <div className='flex items-center justify-between px-6 pt-12 pb-4'>
                 <Button className="" variant='back' onClick={() => navigate(-1)}>←</Button>
                 <span className='text-[#3D2C2C] font-bold'>支出詳細</span>
-                <button onClick={()=> navigate('/main')} className='font-bold cursor-pointer text-[#F47560]'>完了</button>
+                <button onClick={()=> navigate('/transactions')} className='font-bold cursor-pointer text-[#F47560]'>完了</button>
             </div>
 
             {/* category + expense */}
             <div className='flex flex-col gap-4 px-6'>
-                <div className='bg-white bg-opacity-65 rounded-3xl py-6 flex flex-col items-center gap-2'>
+                <div className='bg-white bg-opacity-65 rounded-3xl py-6 flex flex-col items-center gap-2' onClick={()=> setEditModalOpen(true)}>
                     <span className='text-2xl bg-[#FFD9CE] py-3 px-4 rounded-2xl'>{categoryEmoji[filterExpenses?.category] || '💰'}</span>
                     <span className='text-[#7A5555] text-sm bg-[#FFD9CE] py-1 px-4 rounded-3xl font-bold'>{filterExpenses?.category}</span>
                     <span className='text-[#F47560] font-bold text-2xl'>{filterExpenses?.amount.toLocaleString()}円</span>
@@ -55,7 +64,7 @@ const TransactionDetailPage = () => {
 
             {/* Detail */}
             <div className="flex-1 flex flex-col gap-4 px-6">
-                <div className='bg-white bg-opacity-65 rounded-3xl p-6 flex flex-col gap-4 mt-4'>
+                <div className='bg-white bg-opacity-65 rounded-3xl p-6 flex flex-col gap-4 mt-4' onClick={()=> setEditModalOpen(true)}>
                     <div className='flex justify-between items-center border-b border-[#FFD9CE]'>
                         <span className='text-[#B89090] font-bold text-sm'>店名</span>
                         <span className='text-[#3D2C2C] font-bold text-sm'>{filterExpenses?.title}</span>
@@ -79,12 +88,13 @@ const TransactionDetailPage = () => {
 
                 {/* button */}
                 <div className='flex gap-3 mt-auto pb-20'>
-                    <Button variant='outline' fullWidth onClick={() => {}}>✏️ 修正</Button>
+                    <Button variant='outline' fullWidth onClick={() => setEditModalOpen(true)}>✏️ 修正</Button>
                     <Button variant='primary' fullWidth onClick={() => setModalOpen(true)}>🗑 削除</Button>
                 </div>
             </div>
 
             <ConfirmModal isOpen={modalOpen} title='支出を削除しますか？' message='削除した支出は元に戻せません' onConfirm={handleDelete} onCancel={() => setModalOpen(false)}/>
+            <EditModal isOpen={editModalOpen} onClose={()=> setEditModalOpen(false)} onSave={handleUpdate} initialData={{title:filterExpenses?.title, amount:filterExpenses?.amount, memo:filterExpenses?.memo, date:filterExpenses?.expenseDate}}></EditModal>
 
         </div>
         
